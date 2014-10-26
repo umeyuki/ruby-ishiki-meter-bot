aff9fe266854648c4d7e9b0904036f782fc358e5
umeyuki <umeyuki1326+github@gmail.com>
2014-10-26 15:34:04 +0900

    add bot.rb

---
+++ bot.rb
@@ -0,0 +1,94 @@
+#!/usr/bin/env ruby
+
+require 'logger'
+require 'tweetstream'
+require 'ishiki'
+
+include Ishiki
+
+log = Logger.new(STDOUT)
+STDOUT.sync = true
+
+# REST
+rest = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+end
+
+# Stream API
+
+TweetStream.configure do |config|
+  config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+  config.auth_method        = :oauth
+end
+stream = TweetStream::Client.new
+
+EM.error_handler do |e|
+  raise e.message
+end
+
+
+EM.run do
+  # auto folllow and unfollow (every 5 minutes)
+  EM.add_periodic_timer(300) do
+    friends   = rest.friend_ids.all
+    followers = rest.follower_ids.all
+    to_follow = friends - followers
+    to_unfollow = followers - friends
+     # follow
+    log.info('to follow: %s' % to_follow.inspect)
+    to_follow.each do |id|
+      log.info('follow %s' % id)
+      begin
+        if rest.follow(id)
+          log.info('done.')
+        end
+      rescue => e
+        log.error(e)
+      end
+    end
+    #unfollow
+    log.info('to unfollow: %s', to_unfollow.inspect)
+    to_unfollow.each do |id|
+      log.info('unfollow %s' % id)
+      begin
+        if.rest.unfollow(id)
+          log.info('done.')
+        end
+      end
+    end
+  end
+
+  stream.on_inited do
+    log.info('init')
+  end
+  stream.userstream do |status|
+    next if status.retweet?
+    next if status.reply?
+
+    log.info('status @%s: %s' % [status.from_user, status.text])
+    if high_level?(status.text)
+      EM.add_timer(rand(5) + 5) do
+        begin
+          tweet = rest.retweet(status.id)
+          if tweet
+            log.info('tweeted: %s' % tweet.text)
+          end
+        rescue => e
+          log.error(e)
+        end
+      end    
+    end
+
+  end
+  
+end
+
+
+
+
